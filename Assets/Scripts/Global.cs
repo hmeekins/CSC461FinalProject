595856101373cd1affeb90121f92dc0914641dc4
hmeekins <151791982+hmeekins@users.noreply.github.com>
2025-11-22 19:53:18 -0500

    Made a destry script when ball collides with other objects

---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Global
+{
+    public static int score = 0;
+    public static int highScore = 0;
+    public static int lives = 4;
+    public static bool gameActive = false;
+}
