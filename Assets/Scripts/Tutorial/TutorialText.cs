64aafc9477b2505258149b8134e33dbf1da6e1db
hmeekins <151791982+hmeekins@users.noreply.github.com>
2026-04-06 19:47:11 -0400

    Fixed menu issue and updated tutorial text

--- Assets/Scripts/Tutorial/TutorialText.cs
+++ Assets/Scripts/Tutorial/TutorialText.cs
@@ -12,26 +12,31 @@ public class TutorialText : MonoBehaviour
     }
 
     private void DisplayText() {
-        switch(TutorialController.Instance.TutorialStep)
-        {
-            case TutorialStep.GameBasics:
-                _tutorial.text = "Welcome to SpiralVR, your goal is to complete passes to get as high of a score as you can. To start press and hold the front trigger.";
-                break;
-            case TutorialStep.SpawnBall:
-                _tutorial.text = "Now throw the ball at that target. Throw like you normally would and release the trigger the same way you would release the ball.";
-                break;
-            case TutorialStep.HitTarget:
-                _tutorial.text = "Great Job! However, players will be moving in an actual game. Try hitting this moving target.";
-                break;
-            case TutorialStep.HitMovingTarget:
-                _tutorial.text = "Fantastic! You're about ready to play, theres just one more thing I need to mention.";
-                break;
-            case TutorialStep.Defenders:
-                _tutorial.text = "In an actual game you will have defenders following your teammates, if they recieve the ball you lose, there is also a rusher that will run at you, avoid being tackled.";
-                break;
-            case TutorialStep.TutorialOver:
-                _tutorial.text = "You're now ready to begin playing!";
-                break;
-        }
+    switch(TutorialController.Instance.TutorialStep)
+    {
+        case TutorialStep.GameBasics:
+            _tutorial.text = "Welcome to Spiral VR! Your goal is to complete passes and rack up the highest score possible. To begin, press and hold the front trigger.";
+            break;
+
+        case TutorialStep.SpawnBall:
+            _tutorial.text = "Nice! Now throw the ball at the target. Use a natural throwing motion, and release the trigger just like you would release a real ball.";
+            break;
+
+        case TutorialStep.HitTarget:
+            _tutorial.text = "Great job! In real games, your targets won’t stay still. Try hitting this moving target.";
+            break;
+
+        case TutorialStep.HitMovingTarget:
+            _tutorial.text = "Excellent work! You're almost ready to play, just one more thing to know.";
+            break;
+
+        case TutorialStep.Defenders:
+            _tutorial.text = "In a real game, defenders will follow your teammates. If a defender catches the ball, you lose the play. Also watch out for the rusher charging at you or you will be tackled!";
+            break;
+
+        case TutorialStep.TutorialOver:
+            _tutorial.text = "You're all set! Jump in and start playing Spiral VR. Good luck!";
+            break;
     }
+}
 }
