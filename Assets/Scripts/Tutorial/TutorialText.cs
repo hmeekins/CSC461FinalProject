389174b459adfc9f6e2c55acd2d94ba7b01a5074
hmeekins <151791982+hmeekins@users.noreply.github.com>
2026-06-19 10:59:42 -0400

    Changed TutorialText

--- Assets/Scripts/Tutorial/TutorialText.cs
+++ Assets/Scripts/Tutorial/TutorialText.cs
@@ -19,7 +19,7 @@ public class TutorialText : MonoBehaviour
             break;
 
         case TutorialStep.SpawnBall:
-            _tutorial.text = "Great! Now you can throw the ball. Use a natural throwing motion, and release the trigger just like you would release a real ball. See if you can throw to each zone."
+            _tutorial.text = "Great! Now you can throw the ball. Use a natural throwing motion, and release the trigger just like you would release a real ball. See if you can throw to each zone. *See jumbotron for throwing example*"
                 + $"\n\n{TutorialController.Instance.ZonesHit}/3 zones completed";
             break;
 
